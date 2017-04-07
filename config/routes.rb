7221187f8e7de6e9b4d3864427fc42636fce11dc
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-04-07 17:49:44 +0200

    Add welcome page to router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :submissions
-
-  # get '/submissions', to: 'submissions#index'
+  root to: 'submissions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
